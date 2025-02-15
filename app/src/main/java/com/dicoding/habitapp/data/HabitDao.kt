c92ab4a5b8bfd85680dc6315a9a3087707c31370
KrishnanditoLksn <krislaksana123@gmail.com>
2025-02-15 09:26:58 +0700

    feat:dao for habit

--- app/src/main/java/com/dicoding/habitapp/data/HabitDao.kt
+++ app/src/main/java/com/dicoding/habitapp/data/HabitDao.kt
@@ -3,6 +3,9 @@ package com.dicoding.habitapp.data
 import androidx.lifecycle.LiveData
 import androidx.paging.PagingSource
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.RawQuery
 import androidx.sqlite.db.SupportSQLiteQuery
@@ -14,13 +17,18 @@ interface HabitDao {
     @RawQuery(observedEntities = [Habit::class])
     fun getHabits(query: SupportSQLiteQuery): PagingSource<Int, Habit>
 
+    @Query("SELECT * FROM habits WHERE id= :habitId")
     fun getHabitById(habitId: Int): LiveData<Habit>
 
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insertHabit(habit: Habit): Long
 
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insertAll(vararg habits: Habit)
 
+    @Delete
     fun deleteHabit(habits: Habit)
 
+    @Query("SELECT * FROM habits WHERE priorityLevel= :level")
     fun getRandomHabitByPriorityLevel(level: String): LiveData<Habit>
 }
